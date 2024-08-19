4552e73a8a71c8dbdf7f3e13c64e289e5e6ecf5f
bohrstein <kur.esel08@gmail.com>
2024-08-19 06:19:22 -0700

    Create Sarj_baslangıc.py

---
+++ Sarj_baslangıc.py
@@ -0,0 +1,27 @@
+
+def sarj_hesap () 
+    print(self.batarya)
+
+    #Tanımlar 
+    uzaklik = math.sqrt((hedef_enlem-guncel_enlem)**2 + (hedef_boylam-guncel_boylam)**2)
+    mutlak_hedef 
+    uzaklik_sarj
+
+    #hesaplanması gerekenler
+    aktas = Hedefe yaklaştıktan sonra gidecek şarj sabiti(7)
+    iniş anındaki harcanan hız sabitleri (19)
+    kaplan = (cezeri.yukselti - mutlak_hedef.yukselti) * 0.22 = #inis anında harcanacak maksimum şarj
+    al = self.batarya.veri - ((uzaklik-50)* hızlı_giderken_harcadigi_sarj) - kaplan - aktas 
+    hızlı_giderken_harcadigi_sarj = 0.0894736842
+    if uzaklik > 50: 
+        if al < 25:
+            self.dur()
+            sarj_azaldi 
+        elif  al - (uzakllik_sarj * gereklisabitler) < 25: 
+            self.dur()
+            sarj_azaldi
+        else 
+            #sorun yok
+
+
+def sarj_azaldi():
