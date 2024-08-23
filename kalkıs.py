47b772c0db277d77ee67a1d45e60eaee1ae0ecbf
bohrstein <kur.esel08@gmail.com>
2024-08-23 11:34:36 +0300

    Update and rename sarja_git_aa.py to kalkıs.py

---
+++ kalkıs.py
@@ -0,0 +1,20 @@
+
+elif self.barometre.hata == False and self.irtifa_araliginda == False:
+    if self.barometre.irtifa < 10 and self.barometre.irtifa < 100:
+    self.yukari_git(YAVAS)
+    elif self.barometre.irtifa < 30 and self.barometre.irtifa < 100:
+        self.yukari_git(ORTA)
+    elif self.barometre.irtifa < 100:
+        self.yukari_git(HIZLI)
+    else: 
+        self.irtifa_araliginda = True 
+elif self.gnss.hata == False and self.irtifa_araliginda == False:
+    if self.gnss.irtifa < 10:
+        self.yukari_git(YAVAS)
+    elif self.gnss.irtifa < 30:
+        self.yukari_git(ORTA)
+    elif self.gnss.irtifa < 100:
+        self.yukari_git(HIZLI)
+    else: 
+        self.irtifa_araliginda = True 
+elif  self.irtifa_araliginda == False:
